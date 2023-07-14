67cbcb5a3eea57420b0b7f90e739cd6130df0f6f
Hamid-Iqbal-Khan <hamid.it.tech@gmail.com>
2023-07-14 20:34:14 +0530

    first commit

---
+++ src/test/java/com/java/gitOps/SpringBootGitopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.gitOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGitopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
